a5fe53cb4b5929e4dbc29d6859cee4b9b9dc3183
RoodzFernando <fleurantrfernando@gmail.com>
2019-11-20 14:09:59 -0500

    Finished my_each_with_index enumerable

--- enumerables.rb
+++ enumerables.rb
@@ -8,6 +8,15 @@ module Enumerable
         end
     end
 
+    #my_each_with_index 
+
+    def my_each_with_index
+        for i in 0..self.size-1 
+            yield(self[i],i)
+        end
+    end
+
 end
 
-[4, 3, 2, 1, 5, 25, 14, 28].my_each{|n| puts n}
+
+[4, 3, 2, 1, 5, 25, 14, 28].my_each_with_index{|n,m| puts "#{m}:#{n}"}
