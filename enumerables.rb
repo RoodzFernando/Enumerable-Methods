a550d266fc22840a0d0d162c136212ca609dbf68
RoodzFernando <fleurantrfernando@gmail.com>
2019-11-25 12:07:01 -0500

    Fixed all the rubocop errors

--- enumerables.rb
+++ enumerables.rb
@@ -1,177 +1,169 @@
 #!/usr/bin/ruby
+# frozen_string_literal: true
+
+# rubocop:disable ModuleLength
 
 module Enumerable
-    # my_each
-    def my_each
-        if block_given?
-            for i in 0...self.size
-            yield(self[i])
-            end
-        else
-            self.to_enum
-        end
+  # my_each
+  def my_each
+    if block_given?
+      (0...size).each do |i|
+        yield(self[i])
+      end
+    else
+      to_enum
     end
+  end
 
-    #my_each_with_index 
+  # my_each_with_index
 
-    def my_each_with_index
-        if block_given?
-            for i in 0...self.size 
-            yield(self[i],i)
-            end
-        else
-            self.to_enum
-        end
+  def my_each_with_index
+    if block_given?
+      (0...size).each do |i|
+        yield(self[i], i)
+      end
+    else
+      to_enum
     end
+  end
 
-    #my_select
+  # my_select
 
-    def my_select
-        
-        if block_given?
-            if self.instance_of?(Array)
-            temp_array = []
-            self.my_each do |item|
-                temp_array << item if yield(item)
-            end
-            temp_array
-            end
-        else
-            self.to_enum
+  def my_select
+    if block_given?
+      if instance_of?(Array)
+        temp_array = []
+        my_each do |item|
+          temp_array << item if yield(item)
         end
+        temp_array
+      end
+    else
+      to_enum
     end
+  end
 
-    #my_all?
+  # my_all?
 
-    def my_all?
-        if block_given?
-            temp_array = []
-            self.my_each do |item|
-                temp_array << yield(item)
-            end
-            for i in 0...temp_array.size
-             if temp_array[i] == true
-                return  true
-             else
-                return false
-             end
-            end
-        else
-            self.to_enum
-        end
+  def my_all?
+    if block_given?
+      temp_array = []
+      my_each do |item|
+        temp_array << yield(item)
+      end
+      (0...temp_array.size).each do |i|
+        return true if temp_array[i] == true
+
+        return false
+      end
+    else
+      to_enum
     end
+  end
 
-    # my_any?
+  # my_any?
 
-    def my_any?
-        if block_given?
-            temp_array = []
-            self.my_each do |item|
-                temp_array << yield(item)
-            end
-            for i in 0...temp_array.size
-                if temp_array[i] == true || temp_array.nil?
-                    return  true
-                end
-            end
-        else
-            return true
-        end
+  def my_any?
+    if block_given?
+      temp_array = []
+      my_each do |item|
+        temp_array << yield(item)
+      end
+      (0...temp_array.size).each do |i|
+        return true if temp_array[i] == true || temp_array.nil?
+      end
+    else
+      true
     end
+  end
 
-    #my_none?
+  # my_none?
 
-    def my_none?
-        if block_given?
-             temp_array = []
-        self.my_each do |item|
-            temp_array << yield(item)
-        end
-        for i in 0...temp_array.size
-             if temp_array[i] == false || temp_array.nil?
-                return  true
-             end
-        end
-        else
-            return true
-        end
+  def my_none?
+    if block_given?
+      temp_array = []
+      my_each do |item|
+        temp_array << yield(item)
+      end
+      (0...temp_array.size).each do |i|
+        return true if temp_array[i] == false || temp_array.nil?
+      end
+    else
+      true
     end
+  end
 
-    # my_count
+  # my_count
 
-    def my_count
-        if block_given?
-            if self.instance_of?(Array)
-            temp_array = []
-            self.my_each do |item|
-                temp_array << item if yield(item)
-            end
-            temp_array.size
-            end
-        else
-            count = self.size
-        end
+  def my_count
+    if instance_of?(Array)
+      temp_array = []
+      my_each do |item|
+        temp_array << item if yield(item)
+      end
+      temp_array.size
     end
+    return unless block_given?
+  end
 
-    # my_map
+  # my_map
 
-    # def my_map(&block)
-    #    if block_given?
-    #         temp_array = []
-    #         self.my_each do |item|
-    #             temp_array << block.call(item)
-    #         end
-    #         temp_array
-    #         else
-    #         self.to_enum
-    #     end
-    # end
+  # def my_map(&block)
+  #    if block_given?
+  #         temp_array = []
+  #         self.my_each do |item|
+  #             temp_array << block.call(item)
+  #         end
+  #         temp_array
+  #         else
+  #         self.to_enum
+  #     end
+  # end
 
-    # my_inject
+  # my_inject
 
-    def my_inject(param = nil)
-        if block_given?
-        if param.nil?
-            result = self.shift
-            i = 0
-            self.my_each_with_index do |_, index|
-                result = yield(result, self[index])
-            end
-            return result
-        else
-            result = 0
-            i = param
-            for i in i...self.size do
-              result = yield(result, self[i])
-            end
-        end
-        else
-          puts 'No block given'
-        end
-        result
-    end
-
-    # mutiply_els
-
-    def multiply_els(arr)
-        arr.my_inject{|n, p| n * p}
-    end
-
-    # modified my_map enumerable
-
-    def my_map(arr, proc=nil)
-        result = []
-
-        i = 0
-        while i < arr.length
-          if block_given?
-             yield(arr[i])
-          else
-            result << proc.call(arr[i])
-          end
-          i += 1
+  def my_inject(param = nil)
+    if block_given?
+      if param.nil?
+        result = shift
+        my_each_with_index do |_, index|
+          result = yield(result, self[index])
         end
         return result
+      else
+        result = 0
+        i = param
+        (i...size).each do |n|
+          result = yield(result, self[n])
+        end
+      end
+    else
+      puts 'No block given'
     end
+    result
+  end
+
+  # mutiply_els
+
+  def multiply_els(arr)
+    arr.my_inject { |n, p| n * p }
+  end
+
+  # modified my_map enumerable
+
+  def my_map(arr, proc = nil)
+    result = []
+
+    i = 0
+    while i < arr.length
+      if block_given?
+        yield(arr[i])
+      else
+        result << proc.call(arr[i])
+      end
+      i += 1
+    end
+    result
+  end
 end
-    
+# rubocop:enable ModuleLength
