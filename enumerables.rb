c9ba65dbab5a97749cc66d5d433a7333d2edfd71
RoodzFernando <fleurantrfernando@gmail.com>
2019-11-26 12:43:13 -0500

    Fixed all the errors from the code

--- enumerables.rb
+++ enumerables.rb
@@ -2,9 +2,9 @@
 # frozen_string_literal: true
 
 # rubocop:disable ModuleLength
+# rubocop:disable Metrics/PerceivedComplexity,Metrics/CyclomaticComplexity
 
 module Enumerable
-  # my_each
   def my_each
     if block_given?
       (0...size).each do |i|
@@ -15,8 +15,6 @@ module Enumerable
     end
   end
 
-  # my_each_with_index
-
   def my_each_with_index
     if block_given?
       (0...size).each do |i|
@@ -26,8 +24,6 @@ module Enumerable
       to_enum
     end
   end
-
-  # my_select
 
   def my_select
     if block_given?
@@ -43,127 +39,93 @@ module Enumerable
     end
   end
 
-  # my_all?
-
-  def my_all?
+  def my_all?(pattern = nil)
     if block_given?
-      temp_array = []
-      my_each do |item|
-        temp_array << yield(item)
-      end
-      (0...temp_array.size).each do |i|
-        return true if temp_array[i] == true
-
-        return false
-      end
+      my_each { |x| return false unless yield x }
     else
-      to_enum
-    end
-  end
+      return my_all? { |obj| obj } unless pattern
 
-  # my_any?
+      if pattern.class == Regexp
+        my_each { |x| return false unless pattern.match?(x) }
+      elsif pattern.class == Class
+        my_each { |x| return false unless x.is_a? pattern }
+      else
+        my_each { |x| return false unless x == pattern }
+      end
+    end
+    true
+  end
 
   def my_any?
     if block_given?
-      temp_array = []
-      my_each do |item|
-        temp_array << yield(item)
-      end
-      (0...temp_array.size).each do |i|
-        return true if temp_array[i] == true || temp_array.nil?
-      end
+      my_each { |x| return true unless yield x }
     else
-      true
-    end
-  end
+      return my_all? { |obj| obj } unless pattern
 
-  # my_none?
+      if pattern.class == Regexp
+        my_each { |x| return true unless pattern.match?(x) }
+      elsif pattern.class == Class
+        my_each { |x| return true unless x.is_a? pattern }
+      else
+        my_each { |x| return true unless x == pattern }
+      end
+    end
+    false
+  end
 
   def my_none?
     if block_given?
-      temp_array = []
-      my_each do |item|
-        temp_array << yield(item)
-      end
-      (0...temp_array.size).each do |i|
-        return true if temp_array[i] == false || temp_array.nil?
-      end
+      !my_any? { |x| yield x }
+    elsif pattern
+      !my_any?(pattern)
     else
+      my_each { |x| return false if x }
       true
     end
   end
 
-  # my_count
-
-  def my_count
-    if instance_of?(Array)
-      temp_array = []
-      my_each do |item|
-        temp_array << item if yield(item)
-      end
-      temp_array.size
-    end
-    return unless block_given?
-  end
-
-  # my_map
-
-  # def my_map(&block)
-  #    if block_given?
-  #         temp_array = []
-  #         self.my_each do |item|
-  #             temp_array << block.call(item)
-  #         end
-  #         temp_array
-  #         else
-  #         self.to_enum
-  #     end
-  # end
-
-  # my_inject
-
-  def my_inject(param = nil)
+  def my_count(_args = nil)
+    count = 0
     if block_given?
-      if param.nil?
-        result = shift
-        my_each_with_index do |_, index|
-          result = yield(result, self[index])
-        end
-        return result
-      else
-        result = 0
-        i = param
-        (i...size).each do |n|
-          result = yield(result, self[n])
-        end
-      end
+      my_each { |x| count += 1 if yield x }
+    elsif arg
+      my_each { |x| count += 1 if x == arg }
     else
-      puts 'No block given'
+      my_each { count += 1 }
     end
-    result
+    count
   end
 
-  # mutiply_els
+  def my_inject(initial = nil, sym = nil, &block)
+    unless block_given?
+      return my_inject_sym(initial) if initial.class == Symbol
+      return my_inject_sym(sym, initial) if sym
+
+      raise 'No block nor symbol given'
+    end
+    return self[1..length].my_inject(self[0], &block) unless initial
+
+    my_each { |x| initial = block.call(initial, x) }
+    initial
+  end
+
+  def my_inject_sym(sym, initial = nil)
+    return self[1..length].my_inject_sym(sym, self[0]) unless initial
+
+    my_each { |x| initial = initial.send sym, x }
+    initial
+  end
 
   def multiply_els(arr)
     arr.my_inject { |n, p| n * p }
   end
 
-  # modified my_map enumerable
-
-  def my_map(arr, proc = nil)
+  def my_map
     result = []
-
-    i = 0
-    while i < arr.length
-      if block_given?
-        yield(arr[i])
-      else
-        result << proc.call(arr[i])
-      end
-      i += 1
-    end
+    (1..size).each { |n| result << yield(n) } if self.class == Range
+    (1..size).each { |n| result << yield(n) } if self.class != Range
     result
   end
 end
 # rubocop:enable ModuleLength
+# rubocop:enable Metrics/PerceivedComplexity,Metrics/CyclomaticComplexity
